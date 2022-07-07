ef99e2091cb29f95270d604454d797d01a61d9ea
kranthigy <kranthi@goodyear.com>
2022-07-07 15:39:15 -0400

    main

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        // datatype variableName
+        int age;
+        double salaryRequirement;
+        boolean isEmployed;
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+
+        int myInt = 9;
+        double myDouble = myInt;
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
---
+++ out/production/java_basics/README.md
@@ -0,0 +1,2 @@
+# java_basics
+Basics
